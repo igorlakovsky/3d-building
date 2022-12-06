16fe0122669e11b5ab0e98c41d3c241a7a21660b
IgorLakovsky <igorlakovsky@yandex.ru>
2022-12-06 09:25:45 +0400

    react-three-fiber

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Stats } from '@react-three/drei'
+
+function Box(props) {
+  const mesh = useRef()
+
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+
+  useFrame(() => (mesh.current.rotation.x += 0.01))
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={() => setActive(!active)}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <br></br>
+      <Canvas>
+        <Stats />
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
     </div>
   )
 }
